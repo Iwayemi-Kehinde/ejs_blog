9c777b9148efbb259e8a235120b9297e059a8ec2
Iwayemi-Kehinde <iwayemikehinde1@gmail.com>
2024-06-14 09:29:24 +0900

    fixed some bugs

--- models/User.js
+++ models/User.js
@@ -3,12 +3,12 @@ import mongoose from "mongoose"
 const UserSchema = new mongoose.Schema({
  username: {
   type: String,
-  // unique: true,
-  // required: true
+  unique: true,
+  required: true
  },
  password: {
   type: String,
-  // required: true
+  required: true
  }
 })
 
